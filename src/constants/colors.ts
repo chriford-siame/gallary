6bb6c9088130bc0fed02c1cf188f90618eb37cbe
chriford-siame <chrifordsiame@gmail.com>
2024-09-17 14:09:15 +0200

    Add app colors

---
+++ src/constants/colors.ts
@@ -0,0 +1,10 @@
+const COLORS = {
+    white: 'white',
+    secondary: 'gray',
+    dark: 'black',
+    primary: 'rgb(30, 60, 200)',
+    success: 'rgb(10, 100, 100)',
+    danger: 'red',
+    orange: 'darkorange',
+}
+export default COLORS;
