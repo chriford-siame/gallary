57bcdf8ace8c34bb19f32bd9f96e64f1535bc1bc
chriford-siame <chrifordsiame@gmail.com>
2024-10-15 22:57:32 +0200

    register urls

--- App.tsx
+++ App.tsx
@@ -10,18 +10,21 @@ import React from 'react';
 import Home from './src/screens/Home';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
-import Signup from './src/screens/Signup';
-import Login from './src/screens/Login';
+import Signup from './src/screens/auth/Signup';
+import Login from './src/screens/auth/Login';
+import UserProfile from './src/screens/auth/UserProfile';
 
 const Stack = createStackNavigator();
 
 function App(): React.JSX.Element {
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName='login'>
+      <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName='home'>
         <Stack.Screen name='home' component={Home} />
         <Stack.Screen name='signup' component={Signup} />
         <Stack.Screen name='login' component={Login} />
+        <Stack.Screen name='password_reset' component={Login} />
+        <Stack.Screen name='user_profile' component={UserProfile} />
       </Stack.Navigator>
     </NavigationContainer>
   );
