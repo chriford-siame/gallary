fbd97680d3f1aebef9c1a44ee8730e5b49c2d80c
chriford-siame <chrifordsiame@gmail.com>
2024-10-16 12:38:57 +0200

    Clean file

--- App.tsx
+++ App.tsx
@@ -15,6 +15,7 @@ import Login from './src/screens/auth/Login';
 import UserProfile from './src/screens/auth/UserProfile';
 import PasswordReset from './src/screens/auth/PasswordReset';
 import Gallary from './src/screens/gallary/Gallary';
+import GallaryImageView from './src/screens/gallary/GallaryImageView';
 
 const Stack = createStackNavigator();
 
@@ -26,13 +27,12 @@ function App(): React.JSX.Element {
         <Stack.Screen name='signup' component={Signup} />
         <Stack.Screen name='login' component={Login} />
         <Stack.Screen name='password_reset' component={PasswordReset} />
+        <Stack.Screen name='gallary_image_view' component={GallaryImageView} />
         <Stack.Screen name='user_profile' component={UserProfile} />
         <Stack.Screen name='gallary' component={Gallary} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
-// const styles = StyleSheet.create({});
 
 export default App;
