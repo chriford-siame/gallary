01d6033524ec87cac4ea8add7b8a123dbfe3e64c
chriford-siame <chrifordsiame@gmail.com>
2024-10-17 14:34:52 +0200

    update constant data

---
+++ src/constants/folders.ts
@@ -0,0 +1,22 @@
+interface IFolder {
+    name: string
+}
+
+const FOLDERS: IFolder[] = [
+    {
+        name: 'Friends',
+    },
+    {
+        name: 'Family',
+    },
+    {
+        name: 'Luxury',
+    },
+    {
+        name: 'Quotes',
+    },
+    {
+        name: 'Tourism',
+    }, 
+]
+export default FOLDERS;
